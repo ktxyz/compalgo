f22bd271722bf18c6938212c92e5efb44e9c3254
ktxyz <kamil@tokarski.xyz>
2022-11-19 12:27:34 +0100

    add simple, starting template for tasks

---
+++ templates/task.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef PRINT
+#define DEBUG_PRINT(x); printf(x);
+#endif
+
+using ll = long long;
+using ull = unsigned long long;
+using str = std::string;
+template<class T>
+using vec = std::vector<T>;
+template<class T1, class T2>
+using umap = std::unordered_map<T1, T2>;
+
+
+int main() {
+
+    return 0;
+}
